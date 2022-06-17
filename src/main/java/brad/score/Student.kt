53da866bd11263a8a83a0874205d6751a1e3917e
poker555 <brad901017@gmail.com>
2022-06-17 16:33:25 +0800

    6/17 class end

--- src/main/java/brad/score/Student.kt
+++ src/main/java/brad/score/Student.kt
@@ -1,13 +1,23 @@
 package java.brad.score
 
-class Student
+class Student(var name:String,var english:Int,var math:Int)
 {
-    val name = ""
-    val english = 0
-    val math = 0
+    init
+    {
+        //println("Testing")
+    }
+    fun print()
+    {
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage()
+    {
+       (english+math)/2
+    }
 }
 fun main()
 {
     val stu = Student("Jack",55,87)
+    stu.print()
 }
 
